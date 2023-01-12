dd2e955d251bafcc3dfebb0720b5a79ee79a6d74
anacmqui <anacmquintino@gmail.com>
2023-01-12 11:17:21 +0100

    add

--- Dashboard.py
+++ Dashboard.py
@@ -7,7 +7,7 @@ from PIL import Image
 import numpy as np
 
 
-df_2022_ml = pd.read_csv()
+df_2022_ml = pd.read_csv('Documents/GitHub/datathon_realestate/df_2022_ml_st.csv')
 
 tab1, tab2 = st.tabs(["Dashboard", "Machine Learning"])
 
@@ -21,6 +21,8 @@ with tab2:
     col1,col2 = st.columns(2)
     with col1:
         ### Insert Filter 
-        ChooseCountry1 = st.selectbox('Choose your country:', unicorns['Country'].unique(), 1)
-        
+        #ChooseDep1 = st.selectbox('Choose your department:', df_2022_ml['Country'].unique(), 1)
+        ChooseDep1 = st.radio("Choose your department:",df_2022_ml[['75 - Paris','77 - Seine-et-Marne',	'78 - Yvelines',"91 - l'Essonne",	'92 - Hauts-de-Seine',	'93 - Seine-Saint-Denis',
+                                                                    '94 - Val-de-Marne',	"95 - Val-d'Oise"]])
+
     
